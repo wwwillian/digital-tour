8c4d69ce833c472322312f6aadd319b94023bba5
americosly <americo_tss@hotmail.com>
2019-04-22 21:26:48 -0300

    Inserçção dos media queries, menu hamburguer responsivo e cor  de background para white

--- inc/headerdif.php
+++ inc/headerdif.php
@@ -1,7 +1,31 @@
 <header>
-    <nav
-        class="navbar fixed-top navbar-toggleable-md navbar-expand-lg scrolling-navbar navbar-light double-nav navbar-transparente">
-        <a href="index.html" class="navbar-brand">
-            <img src="assets/img/logorev1.png" width="60">
-            <span>Digital Tour</span>
+    <nav class="navbar fixed-top navbar-toggleable-md navbar-expand-lg scrolling-navbar navbar-light double-nav navbar-transparente container-fluid">
+        <!-- <a href="index.html" class="navbar-brand"> -->
+        <img src="assets/img/logorev1.png" width="60">
+        <span>Digital Tour</span>
+        
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+         <span class="navbar-toggler-icon"></span>
+        </button>   
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+            <!-- <a class="navbar-brand" href="#">DIGITAL TOUR</a> -->
+            <ul class="navbar-nav mr-auto mt-2 mt-lg-0">    
+                <li class="nav-item ">
+                <a class="nav-link" href="#">Login</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="#">Quem Somos</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="#">Viagem</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="#">Cadastre-se</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="#">Conheça</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
 </header>
--- inc/headerini.php
+++ inc/headerini.php
@@ -1,7 +1,7 @@
 <div class="conteiner sticky-top">
     <div class="row">
-        <div class="col-lg-12 mx-auto mt-3">
-            <ul class="ul-header mr-5 ">
+        <div class="col-lg-10 mx-auto mt-3">
+            <ul class="navind ul-header mr-5 " id="navind">
                 <li><a href="post.php" class="h5">Conheça</a></li>
                 <li><a href="sobrenos.php" class="h5">Quem Somos</a></li>
                 <li><a href="#" class="h5">Viagem</a></li>
