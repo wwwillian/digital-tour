a83e5335dbf31bf8a935e003fd43469902504397
williangomes7004 <williangomes7004@gmail.com>
2019-06-12 21:45:28 -0300

    Upload dos postos

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Posts\Posts;
+use Illuminate\Http\File;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Input;
@@ -33,35 +34,47 @@ class HomeController extends Controller
 //    }
     public function postsUpdate(Request $request)
     {
+        $data = $request->all();
+
         $request->validate([
             'texttitule',
             'text',
             'photo'
         ]);
-        if(Input::file('photo'))
-        {
+        if(Input::file('photo')) {
             $photo = Input::file('photo');
             $extensao = $photo->getClientOriginalExtension();
+            if ($extensao != 'jpg' && $extensao != 'png' && $extensao != 'jpeg') {
+                return redirect()
+                    ->route('home')
+                    ->with('erro', 'Falha ao fazer o upload da imagem fundo');
+            }
         }
-        if($extensao != 'jpg' && $extensao != 'png' &&  $extensao != 'jpeg')
-        {
-            return redirect()
-                ->route('home')
-                ->with('erro', 'Falha ao fazer o upload da imagem fundo');
-        }
-
         $post = new Posts;
         $post->texttitule = $request->input('texttitule');
         $post->text = $request->input('text');
         $post->release_date = now();
         $post->photo = '';
         $post->save();
-        if(Input::file('photo'))
-        {
-            Input::file('photo')::move($photo,public_path().'/imagem-post/post-id_'.$post->id.'.'.$extensao);
-            $post->photo = public_path().'/imagem-post/post-id_'.$post->id.'.'.$extensao;
-            $post->save();
-        }
+
+            if(Input::file('photo'))
+             {
+                 $name = kebab_case($request->name).uniqid($post->id);
+                 $extension = $request->photo->extension();
+                 $nameImage = "{$name}.$extension";
+                 $data['photo'] = $nameImage;
+
+                 $upload = $request->photo->storeAs('posts', $nameImage);
+
+                 if(!$uploadfundo)
+                     return redirect()
+                         ->route('profile')
+                         ->with('erro', 'Falha ao fazer o upload da imagem fundo');
+
+//            File::move($photo,public_path().'/imagem-post/post-id_'.$post->id.'.'.$extensao);
+//            $post->photo = public_path().'/imagem-post/post-id_'.$post->id.'.'.$extensao;
+//            $post->save();
+            }
 
         return redirect()
             ->route('home')
