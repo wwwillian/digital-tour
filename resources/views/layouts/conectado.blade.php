7a313c03bc159326228290b043eb5bc32ebf59e5
williangomes7004 <williangomes7004@gmail.com>
2019-07-04 00:33:57 -0300

    Ajuste header e footer

--- resources/views/layouts/conectado.blade.php
+++ resources/views/layouts/conectado.blade.php
@@ -15,9 +15,7 @@
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-
     <link href="{{ asset('css/style.css') }}" rel="stylesheet">
-
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.8.1/baguetteBox.min.css">
 
     <script>
