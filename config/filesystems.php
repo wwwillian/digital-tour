bc712c0d0f39d359e26ab39d179d9fbe59b8b95e
williangomes7004 <williangomes7004@gmail.com>
2019-07-03 21:32:18 -0300

    Ajuste link

--- config/filesystems.php
+++ config/filesystems.php
@@ -51,7 +51,7 @@ return [
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'url' => env('tudo').'/storage',
             'visibility' => 'public',
         ],
 
