0a9d96515c5449f622df1a0fbc7f07b34f650ef2
williangomes7004 <williangomes7004@gmail.com>
2019-07-08 19:15:13 -0300

    Ajuste paginas externas

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -28,7 +28,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/feed';
 
     /**
      * Create a new controller instance.
