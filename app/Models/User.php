c4a4516511dccce212791f9eba3589e375323bc1
williangomes7004 <williangomes7004@gmail.com>
2019-08-12 10:22:30 -0300

    Adicionando um outro banco

--- app/Models/User.php
+++ app/Models/User.php
@@ -46,10 +46,10 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
-    public function messages()
-    {
-        return $this->hasMany(Message::class);
-    }
+        public function messages()
+        {
+            return $this->hasMany(Message::class);
+        }
 
     public function posts()
     {
